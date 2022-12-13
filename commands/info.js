17a3e96bde084cf97628312da20fbe5106306762
PewDizinho <konopkspaulo046@gmail.com>
2022-12-13 18:30:21 -0300

    Command and Event handler, Info command added

--- commands/info.js
+++ commands/info.js
@@ -1,42 +1,40 @@
-const { SlashCommandBuilder, ActionRowBuilder,EmbedBuilder,StringSelectMenuBuilder } = require('discord.js');
+const {Message, ComponentType, SlashCommandBuilder, ActionRowBuilder,EmbedBuilder,StringSelectMenuBuilder, Discord } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
+	data: 
+	
+	new SlashCommandBuilder()
 		.setName('info')
 		.setDescription('Informações básicas do servidor!'),
 	async execute(interaction) {
+	
 		const row = new ActionRowBuilder()
 		.addComponents(
 			new StringSelectMenuBuilder()
 					.setCustomId('select')
-					.setPlaceholder('Nothing selected')
+					.setPlaceholder('Selecione algo!')
 					.setMinValues(1)
 					.setMaxValues(1)
 					.addOptions([
 						{
-							label: 'Select me',
-							description: 'This is a description',
-							value: 'first_option',
+							label: 'Pixel Shop',
+							description: 'Mais informações sobre nossa loja virtual!',
+							value: 'pixel_shop',
 						},
 						{
-							label: 'You can select me too',
-							description: 'This is also a description',
-							value: 'second_option',
-						},
-						{
-							label: 'I am also an option',
-							description: 'This is a description as well',
-							value: 'third_option',
-						},
+							label: 'Pixel Community',
+							description: 'Mais informações sobre nossa comunidade!',
+							value: 'pixel_community',
+						}
 					]),
 		);
-
-	const embed = new EmbedBuilder()
+		const embed = new EmbedBuilder()
 		.setColor(0x0099FF)
-		.setTitle('Some title')
-		.setURL('https://discord.js.org/')
-		.setDescription('Some description here');
+		.setTitle('Pixel')
+		.setThumbnail('https://media.discordapp.net/attachments/1052329282069872650/1052329371165274132/Pixel_Coin_Blue.png?width=675&height=675')
+		.setDescription('Escolha as opções abaixo! \n\n<:Pixel_TC:892224665030901800> Pixel Shop\n\n<:Pixel_Coin_Green:1052329968090218617> Pixel Community');
 
-	await interaction.reply({ content: 'Pong!', ephemeral: true, embeds: [embed], components: [row] });
+	await interaction.reply({ content: '', ephemeral: true, embeds: [embed], components: [row] });
+
 	},
 };
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const { token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 const client = new Client({ intents: [GatewayIntentBits.Guilds], presence: "dnd" });
+
+
+//COMMANDS 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -15,9 +18,20 @@ for (const file of commandFiles) {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+////EVENTS
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
@@ -36,5 +50,10 @@ client.on(Events.InteractionCreate, async interaction => {
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
+
+
+
+
+
 
 client.login(token);
