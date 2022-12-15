00ce58763dae7bc637d1d48308f451eaeab55d83
PewDizinho <konopkspaulo046@gmail.com>
2022-12-15 14:07:00 -0300

    .

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -33,3 +33,6 @@ module.exports = {
         await interaction.reply({ content: `Pronto! Sua mensagem foi enviada em ${interaction.options.getChannel("channel")}`, ephemeral: true, })
     }
 }
+
+
+// /broadcast channel:#termos-de-uso titulo::Pixel_Coin_Green: Vendedores :Pixel_Coin_Green:  message::info: Como funciona para vendedores? {breakline}{breakline} :Number_one: Vendedores podem anunciar mediante o comando `/anunciar`, e então deverá escolher entre as categorias: {breakline}:icone_build: Build{breakline}:icone_design: Design{breakline} Mod{breakline}:icone_plugin: Plugin{breakline}:icone_script: Script{breakline}:icone_skript: Skript{breakline} E então uma mensagem com seu produto será enviado no canal específico de seu item, junto de uma descrição, vídeo e preço, caso uma pessoa esteja interessada em realizar a compra, vocês irão 
